17ae2519fb4b5ad0f35c615c8f38ed1961a79342
mvanwaardhuizen <martijn.van.waardhuizen@bumastemra.nl>
2025-01-04 01:20:10 +0100

    ...wip

--- src/services/storage/adapters/storage-aws-s3.service.ts
+++ src/services/storage/adapters/storage-aws-s3.service.ts
@@ -1,13 +1,18 @@
-import { S3Client } from '@aws-sdk/client-s3';
+import { DeleteObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MulterModuleOptions, MulterOptionsFactory } from '@nestjs/platform-express';
 import * as multerS3 from 'multer-s3';
 
 import { DynamicFileInterceptorOptions } from '../interceptors/dynamic-file.interceptor';
+import { StorageEngine } from 'multer';
+import { Request } from 'express';
 
+interface MulterFileWithLocation extends Express.Multer.File {
+  location?: string;
+}
 @Injectable()
-export class AWSS3StorageService implements MulterOptionsFactory {
+export class AWSS3StorageService implements MulterOptionsFactory, StorageEngine {
   protected client: S3Client;
 
   protected readonly DEFAULT_BUCKET_ENV_KEY: string = 'AWS_S3_BUCKET';
@@ -61,5 +66,57 @@ export class AWSS3StorageService implements MulterOptionsFactory {
       this.bucketMap.set(bucket, options);
     }
     return this.bucketMap.get(bucket);
+  }
+
+  async _handleFile(req: Request, file: Express.Multer.File, callback: (error: any, file?: Express.Multer.File) => void): Promise<void> {
+    try {
+      const { originalname, buffer } = file;
+      const bucket = this.getBucket();
+
+      const putCommand = new PutObjectCommand({
+        Bucket: bucket,
+        Key: originalname,
+        Body: buffer,
+      });
+
+      await this.getClient().send(putCommand);
+
+      const fileWithLocation: MulterFileWithLocation = {
+        ...file,
+        location: `https://${bucket}.s3.amazonaws.com/${originalname}`,
+      };
+      callback(null, fileWithLocation);
+    } catch (error) {
+      callback(error);
+    }
+  }
+
+  async removeFile(fileKey: string): Promise<void> {
+    try {
+      const bucket = this.getBucket();
+      const deleteCommand = new DeleteObjectCommand({
+        Bucket: bucket,
+        Key: fileKey,
+      });
+
+      await this.getClient().send(deleteCommand);
+      console.log(`Successfully removed ${fileKey} from bucket ${bucket}`);
+    } catch (error) {
+      console.error(`Failed to remove file from S3: ${fileKey}`, error);
+      throw new Error('File removal failed');
+    }
+  }
+
+  _removeFile(req: Request, file: MulterFileWithLocation, callback: (error: any) => void) {
+    const { location } = file;
+    const fileKey = location.split('/').pop();
+
+    if (fileKey) {
+      this.removeFile(fileKey)
+        .then(() => callback(null))
+        .catch((err) => callback(err));
+    } else {
+      callback(new Error('File key not found'));
+    }
   }
 }
